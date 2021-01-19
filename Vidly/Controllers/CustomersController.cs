ff88d46b0401d7dec08874e4176ee382f9bb1ed8
Lucas Vu <lucas.vu@qorvo.com>
2021-01-19 13:56:23 +0700

    Load customers from the database

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
@@ -7,32 +7,36 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+            //base.Dispose(disposing);
+        }
+
         // GET: Customers
         [Route("customers/{index?}")]
         public ViewResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.Include(c => c.MembershipType);
             return View(customers);
         }
 
         [Route("customers/details/{id}")]
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
 
             return View(customer);
-        }
-
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer{Id = 1, Name = "John Smith"},
-                new Customer{Id = 2, Name = "Lucas Vu"}
-            };
         }
     }
 }
