863a9bdd4e3ad9494f3f615b0cb0784989204c74
Lucas Vu <lucas.vu@qorvo.com>
2021-01-21 18:23:04 +0700

    Complete Section 4.8 Updating data in the database

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -37,6 +38,51 @@ namespace Vidly.Controllers
                 return HttpNotFound();
 
             return View(customer);
+        }
+
+        public ViewResult New()
+        {
+            var viewModel = new CustomerFormViewModel
+            {
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+
+            return View("CustomerForm", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == customer.Id);
+            if (customerInDb == null)
+            {
+                _context.Customers.Add(customer);
+            }
+            else
+            {
+                customerInDb.IsSubcribedToNewsletter = customer.IsSubcribedToNewsletter;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.Name = customer.Name;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+                return HttpNotFound();
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+
+            return View("CustomerForm", viewModel);
         }
     }
 }
--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -9,12 +9,19 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+
+        [Display(Name = "Date of birth")]
         public DateTime? Birthdate { get; set; }
+
         public bool IsSubcribedToNewsletter { get; set; }
+
         public MembershipType MembershipType { get; set; }
+
+        [Display(Name = "Membership type")]
         public byte MembershipTypeId { get; set; }
     }
 }
